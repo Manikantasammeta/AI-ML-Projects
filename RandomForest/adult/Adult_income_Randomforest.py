71970d45e54f852d101f14f6b95031204211967b
Manikantasammeta <manikantasammeta999@gmail.com>
2025-04-28 13:06:08 +0530

    Backend done

--- RandomForest/adult/Adult_income_Randomforest.py
+++ RandomForest/adult/Adult_income_Randomforest.py
@@ -29,6 +29,7 @@ def data_details(df):
     print(df.describe())
     print("The columns of the dataset are ->\n")
     print(df.columns)
+    print("*"*100)
     
 def handle_null_values(df):
     for col in df.columns:
@@ -36,6 +37,7 @@ def handle_null_values(df):
             df[col] = df[col].fillna(df[col].mode()[0])
         else:  # If the column is numerical
             df[col] = df[col].fillna(df[col].mean())
+    print("Null values are handled")
 
     return df
 
@@ -62,18 +64,46 @@ def data_visualization(df):
     sns.boxplot(df['capital_gain'])
     plt.show()
     
+def adding_new_features(df):
+    # df['income']=df['income'].apply(lambda x: '<=50K' if x == '<=50K' else '>50K')
+    df['is_senior'] = df['age'] >= 60 
+    df['net_capital'] = df['capital_gain'] - df['capital_loss']
+    df['work_type'] = df['weekly_hours'].apply(lambda x: 'Full-time' if x >= 35 else 'Part-time')
+
+    df['is_married'] = df['marital_status'].apply(lambda x: 'Married' in x)
+    df['is_native_us'] = df['country_of_origin'] == 'United-States'
+
+    df['income'] = df['income'].apply(lambda x: 0 if x == '<=50K' else 1)
+
+    def categorize_education(num):
+        if num <= 8:
+            return 'Low'
+        elif 9 <= num <= 12:
+            return 'Medium'
+        else:
+            return 'High'
+
+    df['education_level'] = df['education_years'].apply(categorize_education)
+
+    df['experience_estimate'] = df['age'] - df['education_years']
+
+    return df
+
+
+
 
 def data_encoding(df):                                  # Label Encoding
     encoder= LabelEncoder()
     for col in df.columns:
         if df[col].dtype == 'object':
-            df[col] = encoder.fit_transform(df[col])  # Apply label encoding to the column df[col]    
+            df[col] = encoder.fit_transform(df[col])  # Apply label encoding to the column df[col]  
+    print("Encoding is done")  
     return df
 
 
-def data_split(data):
-    X = data.drop('income', axis=1)
-    y = data['income']
+def data_split(df):
+    X = df.drop('income', axis=1)
+    y = df['income']
     
     X_temp, X_check, y_temp, y_check = train_test_split(X, y, test_size=0.1)
 
@@ -81,7 +111,7 @@ def data_split(data):
     print("70% of data for training",X_train.shape)
     print("20% of data for testing",X_test.shape)
     print("10% of data for validation",X_check.shape)
-    
+    print("*"*100)
     return X_train, X_test, X_check, y_train, y_test, y_check
 
 
@@ -91,48 +121,42 @@ def data_standardScaler(X_train, X_test, X_check):
     X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train))
     X_test_scaled = pd.DataFrame(scaler.transform(X_test))
     X_check_scaled = pd.DataFrame(scaler.transform(X_check))
+
     with open("scaler.pkl", "wb") as f:
         pickle.dump(scaler, f)
     
     return X_train_scaled, X_test_scaled, X_check_scaled
 
 def model_training(X_train_scaled ,y_train):
-    models=[
-        RandomForestClassifier(),
-        RandomForestRegressor(),
-        LinearRegression(),
-        DecisionTreeClassifier(),
-        DecisionTreeRegressor(),
-        LogisticRegression()
-    ]
-    trained_models=[]
-    
-    for model in models:
-        model.fit(X_train_scaled,y_train)
-        trained_models.append(model)
-        print(f"Trained Model: {model}\n")
-        print()
-        
-    return trained_models
+    model=RandomForestRegressor()
+    model.fit(X_train_scaled,y_train)
+    print("Models are trained\n",model)
+    print("*"*100)
+    return model
 
-def moel_evaluation(models,X_test_scaled,y_test):
-    for model in models:
+def moel_evaluation(model,X_test_scaled,y_test):
+        print("*"*15,"Model Evalution","*"*15)
         print(f"Model: {model}")
         print("Accuracy:", model.score(X_test_scaled, y_test))
-        print("Log Loss:", log_loss(y_test, model.predict_proba(X_test_scaled)))
-        print("Confusion Matrix:\n", confusion_matrix(y_test, model.predict(X_test_scaled)))
+        # print("Log Loss:", log_loss(y_test, model.predict_proba(X_test_scaled)))                            getting erroe
+        print("Confusion Matrix:\n", confusion_matrix(y_test, model.predict(X_test_scaled),labels = [0, 1] ) )
         print("Classification Report:\n", classification_report(y_test, model.predict(X_test_scaled)))
         print("Accuracy:", accuracy_score(y_test, model.predict(X_test_scaled)))
+        print("*"*100)
         print()
         
-def model_prediction(models,X_check_scaled,y_check):
-    for model in models:
-        print(f"Model: {model}")
+def model_prediction(model,X_check_scaled,y_check):
+       
         print("Actual:\n",np.array(y_check))
         print("Predictions:\n", model.predict(X_check_scaled))
+        are_equal = np.all(np.array(y_check) == np.array(model.predict(X_check_scaled)))
+        print("All Predections are Correct...?",are_equal)  # Will print True if all are equal, False otherwise
+
+       
         print("Accuracy:", accuracy_score(y_check, model.predict(X_check_scaled)))
         print(" Model Score:", model.score(X_check_scaled, y_check))
         print("\n Classification Report:\n", classification_report(y_check, model.predict(X_check_scaled)))
+        print("*"*100)
         print()
     
 
@@ -148,7 +172,7 @@ def save_model(model):
     
 def main():
     df=data_loading()
-    data_details(df)
+    
     data_details(df)
     df.columns = ['age','job_type','person_weight','education_level','education_years',
             'marital_status','job_role','family_role','ethnicity','gender','capital_gain',
@@ -156,13 +180,20 @@ def main():
     print(df.columns)
     if df.isnull().sum().sum() > 0:
         handle_null_values(df)
-    # #data_visualization(df)
-    # df=data_encoding(df)
-    # X_train, X_test, X_check, y_train, y_test, y_check=data_split(df)
-    # X_train_scaled, X_test_scaled, X_check_scaled=data_standardScaler(X_train, X_test, X_check)
-    # models=model_training(X_train_scaled ,y_train)
-    # moel_evaluation(models,X_test_scaled,y_test)
-    # model_prediction(models,X_check_scaled,y_check)
+   
+    df=adding_new_features(df)
+    if df.isnull().sum().sum() > 0:
+        handle_null_values(df)
+    
+    #data_visualization(df)
+    df=data_encoding(df)
+    X_train, X_test, X_check, y_train, y_test, y_check=data_split(df)
+    
+    X_train_scaled, X_test_scaled, X_check_scaled=data_standardScaler(X_train, X_test, X_check)
+    
+    models=model_training(X_train_scaled ,y_train)
+    moel_evaluation(models,X_test_scaled,y_test)
+    model_prediction(models,X_check_scaled,y_check)
     
     
     
